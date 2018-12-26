6555cc6235894538a555231397f166d6754fe599
REÇ <ecoban@sutas.com.tr>
2018-12-26 13:26:01 +0300

    first template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      // <div className="container">
+      //     <div className="item item--1">1: Orange</div>
+      //     <div className="item item--2">2: Green</div>
+      //     <div className="item item--3">3: Violet</div>
+      //     <div className="item item--4">4: Pink</div>
+      //     <div className="item item--5">5: Blue</div>
+      //     <div className="item item--6">6: Brown</div>
+      // </div>
+      <div className="container">
+        <div className="item item__header">Header</div>
+        <div className="item item__small-box-1">Small box 1</div>
+        <div className="item item__small-box-2">Small box 2</div>
+        <div className="item item__small-box-3">Small box 3</div>
+        <div className="item item__slide-bar">Slidebar</div>
+        <div className="item item__main-content">Main Content</div>
+        <div className="item item__footer">Footer</div>
       </div>
     );
   }
